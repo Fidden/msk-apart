a4f22f96328641b3c7ec5f9ecf5870d7f7ad407f
storm <sahalisa17@gmail.com>
2023-02-05 15:50:49 +0300

    fix: Лого теперь ссылка.

--- src/client/shared/components/header/header.tsx
+++ src/client/shared/components/header/header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import {FC} from 'react';
 import {HeaderControls} from './__controls/header__controls';
 import {HeaderNav} from './__nav/header__nav';
@@ -8,13 +9,17 @@ import {cnHeader} from './header.const';
 export const Header: FC = () => {
     return (
         <header className={cnHeader()}>
-            <Image
-                width={92}
-                height={16}
+            <Link
+                href={'/'}
                 className={cnHeader('logo')}
-                src={Logo}
-                alt={'msk apart'}
-            />
+            >
+                <Image
+                    width={92}
+                    height={16}
+                    src={Logo}
+                    alt={'msk apart'}
+                />
+            </Link>
             <HeaderNav/>
             <HeaderControls/>
         </header>
