1db764935da577fc4404963fca9db560b994a06f
storm <sahalisa17@gmail.com>
2023-02-05 18:09:53 +0300

    feat: Добавлены husky, lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import {IndexScreen} from '~/client/screens/index-screen/index-screen';
 
-IndexScreen.getInitialProps = async (context) => {
+IndexScreen.getInitialProps = async () => {
     const res = await fetch('https://api.github.com/repos/vercel/next.js');
     const json = await res.json();
     return {stars: json.stargazers_count};
