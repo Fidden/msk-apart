f8b5b7f99d22e6237dfa7446f3b68fee5410dfe1
storm <sahalisa17@gmail.com>
2023-02-06 21:04:51 +0300

    fix: Исправлен бордер у карты;

--- src/client/screens/contacts-screen/__map/contacts-sceen__map.tsx
+++ src/client/screens/contacts-screen/__map/contacts-sceen__map.tsx
@@ -8,5 +8,6 @@ export const ContactsScreenMap: FC = () => (
         src="https://yandex.ru/map-widget/v1/?um=constructor%3A6bf2128edac5807764ebe3ff6f94e16ef19264b2710f7157660f39a9ffb7068c&amp;source=constructor"
         width="100%"
         height="616"
+        frameBorder="0"
     />
 );
