a3300b884d36b899d430c552165829dc12dbe065
storm <sahalisa17@gmail.com>
2023-02-19 19:43:55 +0300

    feat: Получение данных с сервера.

--- apps/frontend/src/pages/product/[id].tsx
+++ apps/frontend/src/pages/product/[id].tsx
@@ -1,3 +1,16 @@
 import {ProductScreen} from '~/client/screens/product-screen/product-screen';
+import {IProduct} from '~/client/shared/types/product';
+import {IResponse} from '~/client/shared/types/response';
+import {axiosInstance} from '~/pages/_app';
 
+ProductScreen.getInitialProps = async (context) => {
+    const productId = context.query.id;
+    const product = await axiosInstance.get<IResponse<IProduct>>(`/product/${productId}`);
+    const special = await axiosInstance.get<IResponse<IProduct[]>>(`/special`);
+
+    return {
+        product: product.data.data,
+        special: special.data.data
+    };
+};
 export default ProductScreen;
