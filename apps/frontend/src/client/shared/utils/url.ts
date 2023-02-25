1c1dacc45f50a4be385866696e5e75e762d6cd05
storm <sahalisa17@gmail.com>
2023-02-25 14:26:08 +0300

    feat: Добавлен реквест билдер.

---
+++ apps/frontend/src/client/shared/utils/url.ts
@@ -0,0 +1,27 @@
+import {AxiosRequestConfig} from 'axios';
+import {axiosInstance} from '~/pages/_app';
+
+export class Url<Params extends Record<string, string>, Query extends Record<string, any>> {
+    private path: string;
+
+    constructor(path: string) {
+        this.path = path;
+    }
+
+    // get params of url and replace :param with variable
+    public build<T>(pathParams?: Params, pathQuery?: Query, options?: AxiosRequestConfig) {
+        const path = this.path.replace(/:([a-zA-Z0-9_]+)/g, (match, param) => {
+            if (pathParams && pathParams[param]) {
+                return pathParams[param];
+            }
+
+            return match;
+        });
+
+        // generate axios request with params and query
+        return axiosInstance<T>(path, {
+            ...options,
+            params: pathQuery
+        });
+    }
+}
---
+++ apps/frontend/src/client/shared/utils/urls.ts
@@ -0,0 +1,7 @@
+import {Url} from '~/client/shared/utils/url';
+
+export const urls = {
+    product: new Url('/product/:id'),
+    productInfo: new Url('/info'),
+    special: new Url('/special')
+};
