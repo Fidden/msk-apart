85e48d2a23ee33cf466dde5ba7bc5ed7306e1744
rrpvm <funikovsasa@gmail.com>
2023-02-25 19:33:50 +0700

    feat: Добавил компонент чекбокс.

---
+++ apps/frontend/src/client/shared/components/checkbox/checkbox.const.ts
@@ -0,0 +1,3 @@
+import './checkbox.css';
+import block from 'bem-cn';
+export const cnCheckbox = block('checkbox');
---
+++ apps/frontend/src/client/shared/components/checkbox/checkbox.tsx
@@ -0,0 +1,28 @@
+import {FC, PropsWithChildren, useRef} from "react";
+import {cnCheckbox} from './checkbox.const';
+
+interface ICheckboxProps extends PropsWithChildren {
+    onChange?: (checked: boolean) => void;
+}
+
+export const Checkbox: FC<ICheckboxProps> = (props) => {
+    const checked = useRef<boolean>(false);
+    const handleChange = () => {
+        checked.current = !checked.current;
+        if (props.onChange) {
+            props.onChange(checked.current);
+        }
+    };
+
+    return (
+        <label className={cnCheckbox()}>
+            {props.children}
+            <input
+                type={"checkbox"}
+                className={cnCheckbox("input")}
+                onChange={handleChange}
+            />
+            <span className={cnCheckbox("item")}/>
+        </label>
+    );
+};
