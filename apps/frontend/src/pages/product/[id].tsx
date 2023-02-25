859cdb4fe3977a468d599fac8c9be92c8b0a61b9
storm <sahalisa17@gmail.com>
2023-02-25 14:27:34 +0300

    refactor: Построение реквеста билдером.

--- apps/frontend/src/pages/product/[id].tsx
+++ apps/frontend/src/pages/product/[id].tsx
@@ -1,16 +1,19 @@
 import {ProductScreen} from '~/client/screens/product-screen/product-screen';
 import {IProduct} from '~/client/shared/types/product';
 import {IResponse} from '~/client/shared/types/response';
-import {axiosInstance} from '~/pages/_app';
+import {urls} from '~/client/shared/utils/urls';
 
 ProductScreen.getInitialProps = async (context) => {
-    const productId = context.query.id;
-    const product = await axiosInstance.get<IResponse<IProduct>>(`/product/${productId}`);
-    const special = await axiosInstance.get<IResponse<IProduct[]>>(`/special`);
+    const productId = context.query.id as string;
+    const [product, special] = await Promise.all([
+        await urls.product.build<IResponse<IProduct>>({id: productId}),
+        await urls.special.build<IResponse<IProduct[]>>()
+    ]);
 
     return {
         product: product.data.data,
         special: special.data.data
     };
 };
+
 export default ProductScreen;
