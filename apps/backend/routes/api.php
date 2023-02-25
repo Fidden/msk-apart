fc3abe5bdb0e11299ae8ee359e5cbbc790ddcc10
storm <sahalisa17@gmail.com>
2023-02-25 14:25:27 +0300

    feat: Добавлен роут для вывода информации.

--- apps/backend/routes/api.php
+++ apps/backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductSpecialController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/product')->group(function () {
@@ -9,5 +10,12 @@ Route::prefix('/product')->group(function () {
     Route::get('/{product}', [ProductController::class, 'show']);
 });
 
+Route::get('/info', [ProductController::class, 'info']);
 Route::get('/special', [ProductSpecialController::class, 'index']);
+
+Route::prefix('/user')->group(function () {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+    Route::any('/logout', [UserController::class, 'logout']);
+});
 
